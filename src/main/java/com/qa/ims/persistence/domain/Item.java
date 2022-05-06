9a75dbabac3fb93789716c3fee2ae181f53752fe
Rayyana <sulrayy@gmail.com>
2022-05-06 12:42:29 +0100

    added Item domain getters, setters and constructors

--- src/main/java/com/qa/ims/persistence/domain/Item.java
+++ src/main/java/com/qa/ims/persistence/domain/Item.java
@@ -6,5 +6,44 @@ public class Item {
 	private String name;
 	private double price;
 	
+	public Item(String name, double price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	public Item(long id, String name, double price) {
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	@Override
+	public String toString() {
+		return "id: " + id + ", name: " + name + ", price (£):" + price;
+	}
 
 }
