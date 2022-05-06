11cc6bfc81cfae47c4cdb18a06cfe8a133b7db9a
Rayyana0 <103116830+Rayyana0@users.noreply.github.com>
2022-05-06 14:27:03 +0100

    Update sql-schema.sql

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -10,3 +10,25 @@ CREATE TABLE IF NOT EXISTS `ims`.`customers` (
     `surname` VARCHAR(40) DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
+
+CREATE TABLE IF NOT EXISTS `ims`.`items` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(40) DEFAULT NULL,
+    `price` DOUBLE DEFAULT NULL,
+    `orders_item_id` INT(11) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `ims`.`orders` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `customers_id` INT(11) DEFAULT NULL,
+    `orders_item_id` INT(11) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `ims`.`orders_items` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `orders_id` INT(11) DEFAULT NULL,
+    `items_id` INT(11) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+);
